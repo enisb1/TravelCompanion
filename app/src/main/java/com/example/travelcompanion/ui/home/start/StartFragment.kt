f58cb2ddf9ec62d7a2b020ee23d42c1e8fdd53d9
enisb1 <b.encio.003@gmail.com>
2025-04-19 15:14:15 +0200

    add/show map when pressing start

--- app/src/main/java/com/example/travelcompanion/ui/home/start/StartFragment.kt
+++ app/src/main/java/com/example/travelcompanion/ui/home/start/StartFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.FragmentContainerView
 import com.example.travelcompanion.R
+import com.google.android.gms.maps.SupportMapFragment
 
 class StartFragment : Fragment() {
 
@@ -21,5 +24,19 @@ class StartFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val startButton: Button = view.findViewById(R.id.startButton)
+        val fgContainer: FragmentContainerView = view.findViewById(R.id.mapContainer)
+        startButton.setOnClickListener {
+            startButton.visibility = View.GONE
+            fgContainer.visibility = View.VISIBLE
+
+            val mapFragment: SupportMapFragment =
+                childFragmentManager.findFragmentById(R.id.mapContainer) as SupportMapFragment
+            mapFragment.getMapAsync {
+                
+            }
+
+        }
     }
 }
