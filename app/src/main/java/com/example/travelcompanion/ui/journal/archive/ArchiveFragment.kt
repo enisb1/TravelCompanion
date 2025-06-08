cafd0880d2033fc2e331c9844d56f1e3115aaab9
enisb1 <b.encio.003@gmail.com>
2025-06-08 12:10:23 +0200

    add trip selection spinner to archive

--- app/src/main/java/com/example/travelcompanion/ui/journal/archive/ArchiveFragment.kt
+++ app/src/main/java/com/example/travelcompanion/ui/journal/archive/ArchiveFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.travelcompanion.R
+import pl.utkala.searchablespinner.SearchableSpinner
+import pl.utkala.searchablespinner.StringHintArrayAdapter
 
 class ArchiveFragment : Fragment() {
 
@@ -14,18 +16,26 @@ class ArchiveFragment : Fragment() {
         fun newInstance() = ArchiveFragment()
     }
 
-    private val viewModel: ArchiveViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    private lateinit var tripSelectionSpinner: SearchableSpinner
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_archive, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tripSelectionSpinner = view.findViewById(R.id.trip_selection_spinner)
+        tripSelectionSpinner.showHint = true
+        tripSelectionSpinner.adapter= StringHintArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_dropdown_item,
+            listOf("prova", "test", "daje"),
+            "Select Trip"
+        )
+
     }
 }
