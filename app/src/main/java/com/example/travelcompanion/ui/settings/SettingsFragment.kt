3b9264602560e0a17cb9296d913ecb3975bfdc71
enisb1 <b.encio.003@gmail.com>
2025-06-12 11:45:46 +0200

    display buttons to choose activity recognition means of transport

--- app/src/main/java/com/example/travelcompanion/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/travelcompanion/ui/settings/SettingsFragment.kt
@@ -7,12 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.ImageView
 import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
 import com.example.travelcompanion.R
 import androidx.core.content.edit
 
 class SettingsFragment : Fragment() {
+
+    companion object {
+        const val TRACK_CAR = "trackCar"
+        const val TRACK_BICYCLE = "trackBicycle"
+        const val TRACK_RUNNING = "trackRunning"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +34,9 @@ class SettingsFragment : Fragment() {
         val etTrips = view.findViewById<EditText>(R.id.etMonthlyTripsGoal)
         val etDistance = view.findViewById<EditText>(R.id.etMonthlyDistanceGoal)
         val btnSave = view.findViewById<Button>(R.id.btnSaveGoals)
+        val carLayout = view.findViewById<FrameLayout>(R.id.car_layout)
+        val bicycleLayout = view.findViewById<FrameLayout>(R.id.bicycle_layout)
+        val runningLayout = view.findViewById<FrameLayout>(R.id.running_layout)
 
         // Load existing objectives if available
         etTrips.setText(prefs.getInt("monthlyTripsGoal", 0).takeIf { it > 0 }?.toString() ?: "")
@@ -40,8 +50,28 @@ class SettingsFragment : Fragment() {
             prefs.edit {
                 putInt("monthlyTripsGoal", tripsGoal)
                     .putInt("monthlyDistanceGoal", distanceGoal)
+                    .putBoolean(TRACK_CAR, carLayout.isSelected)
+                    .putBoolean(TRACK_BICYCLE, bicycleLayout.isSelected)
+                    .putBoolean(TRACK_RUNNING, runningLayout.isSelected)
             }
             Toast.makeText(requireContext(), "Objectives set!", Toast.LENGTH_SHORT).show()
+        }
+
+        // activity recognition
+        val trackCar = prefs.getBoolean(TRACK_CAR, false)
+        val trackBicycle = prefs.getBoolean(TRACK_BICYCLE, false)
+        val trackRunning = prefs.getBoolean(TRACK_RUNNING, false)
+        if (trackCar)
+            carLayout.isSelected = true
+        if (trackBicycle)
+            bicycleLayout.isSelected = true
+        if (trackRunning)
+            runningLayout.isSelected = true
+
+        for (frameLayout in listOf(carLayout, bicycleLayout, runningLayout)) {
+            frameLayout.setOnClickListener {
+                it.isSelected = !it.isSelected
+            }
         }
     }
 }
